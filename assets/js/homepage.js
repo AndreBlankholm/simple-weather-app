b0c4b850c1f71fae3ce3bddc2ced9f5d724925e8
AndreBlankholm <ablankholm@gmail.com>
2022-11-24 18:29:36 -0600

    working on the Fetch call

---
+++ assets/js/homepage.js
@@ -0,0 +1,24 @@
+
+
+
+var weatherData = function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?zip=55016,us&appid=0251118249ac618dd0a66cb0a818b7fd"
+  ).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      displayWeather(data, city);
+    });
+  });
+
+  console.log("outside");
+};
+
+var displayWeather = function(city, name) {
+  console.log(city);
+  console.log(name);
+  
+};
+
+
+weatherData();
