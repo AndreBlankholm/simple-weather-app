28094f86772b3aca59f696eb7b3b913c010dc8b0
AndreBlankholm <ablankholm@gmail.com>
2022-11-30 16:58:25 -0600

    function seperation

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,22 @@
 var currentLocalEl = document.querySelector("#current-location-container");
 var currentTempEl = document.querySelector("#current-temp-container");
 
+var getDisplayDegrees = function (kalvinTemp) {
+  var kavinToFahrenheitConversion = (kalvinTemp - 273.15) * 1.8 + 32;
+  var displayDegrees = kavinToFahrenheitConversion.toFixed(1);
+  return displayDegrees;
+};
+
+var updateUIElements = function (cityName, degrees) {
+  currentLocalEl.textContent = "";
+  currentLocalEl.textContent =
+    "I'm currently located in " +
+    cityName +
+    " and the current temp here is " +
+    degrees +
+    " degrees F.";
+};
+
 var weatherData = function () {
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?zip=55016,us&appid=0251118249ac618dd0a66cb0a818b7fd"
@@ -10,16 +26,8 @@ var weatherData = function () {
       //console.log(data.city.name);
       var cityName = data.city.name;
       var kalvinTemp = data.list[0].main.temp_max;
-      var kavinToFahrenheitConversion = (kalvinTemp - 273.15) * 1.8 + 32;
-      var displaydegress = kavinToFahrenheitConversion.toFixed(1);
-
-      currentLocalEl.textContent = "";
-      currentLocalEl.textContent =
-        "I'm currently located in " +
-        cityName +
-        " and the current temp here is " +
-        displaydegress +
-        " degrees F.";
+      var displayDegrees = getDisplayDegrees(kalvinTemp);
+      updateUIElements(cityName, displayDegrees);
     });
   });
 };
