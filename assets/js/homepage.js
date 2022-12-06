dca0ddd50b9bd34508b246522c88f52ff654c7a4
AndreBlankholm <ablankholm@gmail.com>
2022-12-05 19:37:28 -0600

    added geolocation call

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,22 @@
 var currentLocalEl = document.querySelector("#current-location-container");
 var currentTempEl = document.querySelector("#current-temp-container");
 
+var getWeatherData = function() {
+ getGeoLocation();
+
+};
+
+var getGeoLocation = function() {
+  window.navigator.geolocation.getCurrentPosition(function(response) {
+    console.log(response);
+    var lat = response.coords.latitude;
+    var lon = response.coords.longitude;
+    weatherData(lat, lon);
+}, function(error) {
+    alert(error);
+});
+};
+
 var getDisplayDegrees = function (kalvinTemp) {
   var kavinToFahrenheitConversion = (kalvinTemp - 273.15) * 1.8 + 32;
   var displayDegrees = kavinToFahrenheitConversion.toFixed(1);
@@ -10,26 +26,27 @@ var getDisplayDegrees = function (kalvinTemp) {
 var updateUIElements = function (cityName, degrees) {
   currentLocalEl.textContent = "";
   currentLocalEl.textContent =
-    "I'm currently located in " +
+    "You are currently located in " +
     cityName +
-    " and the current temp here is " +
+    " and the current temp there is " +
     degrees +
     " degrees F.";
 };
 
-var weatherData = function () {
+var weatherData = function (lat, lon) {
+  
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?zip=55016,us&appid=0251118249ac618dd0a66cb0a818b7fd"
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0251118249ac618dd0a66cb0a818b7fd`
   ).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
       //console.log(data.city.name);
-      var cityName = data.city.name;
-      var kalvinTemp = data.list[0].main.temp_max;
+      var cityName = data.name;
+      var kalvinTemp = data.main.temp_max;
       var displayDegrees = getDisplayDegrees(kalvinTemp);
       updateUIElements(cityName, displayDegrees);
     });
   });
 };
 
-weatherData();
+getWeatherData();
